0ae11ef8ca2c673f927e657e75ad1b0488cb20a4
Sarthak Dua <sarthakdua@197NODMB24439.local>
2021-06-18 00:51:45 +0530

    Initial commit with authentication

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Signup from "./authentication/Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from "./authentication/Login"
+import PrivateRoute from "./authentication/PrivateRoute"
+import ForgotPassword from "./authentication/ForgotPassword"
+import UpdateProfile from "./authentication/UpdateProfile"
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App
